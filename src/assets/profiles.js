e12bb9c4795e4044feb5d202d91bbd5fc24d02a4
Rebeckawestborg <rwestborg@gmail.com>
2025-04-17 13:21:00 +0200

    oppdatert info om Rebecka

--- src/assets/profiles.js
+++ src/assets/profiles.js
@@ -19,12 +19,12 @@ const profiles = [
     },
     {
       id: 'rebecka',
-      fornavn: '?',
-      etternavn: '?',
-      epost: '?@hiof.no',
+      fornavn: 'Rebecka',
+      etternavn: 'Westborg',
+      epost: 'rebeckw@hiof.no',
       bilde: '??.png',
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      interesser: ['bla', 'bla', 'bla'],
+      bio: 'Mitt navn er Rebecka Westborg og jeg er en jente på 21 år som bor i Halden. Der har jeg bodd i snart 7 år. Jeg bodde i Sarpsborg før vi flyttet til Halden, men jeg ble født og oppvokst i Fredrikstad egentlig. Man kan si jeg er årntli østlending. Jeg bor med mamma, pappa, mine 2 søstre, bestemoren min som jeg kaller Mimmi, og en liten hund som heter Tinka.',
+      interesser: ['tegne', 'male', 'grafisk design', 'fotografi'],
     },
     {
       id: 'mohammed',
