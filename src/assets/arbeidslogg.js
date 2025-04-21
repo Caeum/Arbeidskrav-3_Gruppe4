e85a4688dd608187322be178eb7dfde7ad4e5966
MohammedY03 <mohammey@hiof.no>
2025-04-21 20:54:45 +0200

    arbeidslogg
    lagt til arbeidslogg

--- src/assets/arbeidslogg.js
+++ src/assets/arbeidslogg.js
@@ -58,6 +58,59 @@ const arbeidslogg = [
     timer: '3 time'
   },
 
+
+  {
+    dato: '2025-04-7',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Hjalp til med å opprette "HomePage.jsx" komponenten',
+    timer: '1 time'
+  },
+  {
+    dato: '2025-04-17',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Hjulpet til ved å legge på biografi.',
+    timer: '1 time'
+  },
+  {
+    dato: '2025-04-19',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Lagt til className i komponenter, og jobbet med css',
+    timer: '2 timer'
+  },
+  {
+    dato: '2025-04-20',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Satt opp css på biografien og finjustert siden for at det skal se ryddigere ut.',
+    timer: '2 timer'
+  },
+  {
+    dato: '2025-04-21',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'La "biografien" og "interesser" ved siden av bildene ved hjelp av float propertiet.',
+    timer: '3 timer'
+  },
+  {
+    dato: '2025-04-21',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Lagt til media queries for å gjøre nettsiden mer responsiv.',
+    timer: '1 time'
+  },
+  {
+    dato: '2025-04-21',
+    navn: 'Mohammed Shirco Yousif',
+    id: 'mohammed',
+    oppgave: 'Hjalp til med å legge til logg i arbeidsloggen.',
+    timer: '1 time'
+  },
+  
+  
+
 ];
 
   export default arbeidslogg;
